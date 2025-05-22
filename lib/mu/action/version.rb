b11defdcd8a539442bd9574c441b0911b5b437b1
Nicolas Buduroi <nbuduroi@gmail.com>
2025-05-21 23:10:02 -0400

    chore: initial gem project setup using bundler

---
+++ lib/mu/action/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Mu
+  module Action
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/mu/action_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Mu::Action do
+  it "has a version number" do
+    expect(Mu::Action::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
