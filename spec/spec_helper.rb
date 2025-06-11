f49fc09f93f5a27d0f773118c001d206e45dd5f8
Nicolas Buduroi <nbuduroi@gmail.com>
2025-06-10 20:48:58 -0400

    chore: add focus support for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@
 require "mu/action"
 
 RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
